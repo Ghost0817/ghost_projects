2ffc3f238b51f0054d5bbb5c5015029b5688a187
ghost0817 <sainzaya.b@gmail.com>
2019-11-23 11:33:57 +0800

    svg test opuulaw.

---
+++ bchch/src/Controller/Blog/BlogController.php
@@ -0,0 +1,10 @@
+<?php
+// src/Controller/Blog/BlogController.php
+namespace App\Controller\Blog;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+}
